a376ee8b589bf22500e67be52ac545e0e1c87f81
Muhammad Nauman <maliknomigep@gmail.com>
2026-05-11 19:43:36 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[5];
+    arr[0]=1;
+    arr[1]=9;
+    arr[2]=7;
+    arr[3]=65;
+    arr[4]=91;
+    cout<<arr[0]<<endl;
+    cout<<arr[1]<<endl;
+    cout<<arr[2]<<endl;
+    cout<<arr[3]<<endl;
+    cout<<arr[4]<<endl;
+    return 0;
+}
